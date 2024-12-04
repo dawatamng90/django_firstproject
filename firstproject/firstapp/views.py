95f0b723f71a165c0215a51025f80abd5f1c4a5e
Dawa <dawatamng90@gmail.com>
2024-12-04 21:52:38 +0530

    Second commit by Dawa

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def display(request):
+    print("This line is added at 21:52 04-12-2024")
     return HttpResponse('<h2>Hello, Students. Welcome to Django tutorials.</h2>')
